ebf70dec9dda2c2d723c54286c135f4039007602
sujithga <sujith.ga@gmail.com>
2025-06-11 13:51:14 +0530

    updated the logic

--- src/main/java/org/sujith/Ch01FizzBuzz.java
+++ src/main/java/org/sujith/Ch01FizzBuzz.java
@@ -3,7 +3,7 @@ package org.sujith;
 public class Ch01FizzBuzz {
 
     public static void main(String[] args){
-
+        print(100);
     }
 
     private static void print(int limit) {
@@ -11,7 +11,14 @@ public class Ch01FizzBuzz {
 
             if (i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
+            } else if (i % 3 == 0 ){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
+
 
         }
 
